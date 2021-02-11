ad1ee405c4fb686a72046fa2693bf07d628ffda5
Anna <zykova.anna.zao@gmail.com>
2021-02-11 17:17:32 +0200

    fixes

--- src/test/java/tests/findCodeJunit5Test.java
+++ src/test/java/tests/findCodeJunit5Test.java
@@ -3,30 +3,31 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class findCodeJunit5Test {
 
     @Test
-    void junit5ExistsInSelenideWiki() {
+    void Junit5ExistsInSelenideWiki() {
 
         // Open Github
         // Type Selenide in search
         // Find Selenide/Selenide
         open("https://github.com");
         $("[name=q]").setValue("selenide").pressEnter();
-        $$("ul.repo-list li").first().$("a").click();
+        $(".repo-list a").click();
         $("h1").shouldHave(text("selenide / selenide"));
 
         // Find Wiki on the page
         // Open Wiki
-        $("nav", 1).$("ul").$("li", 4).$("a").click();
+        $("nav", 1).$(byText("Wiki")).click();
         $("#wiki-wrapper").$("h1").shouldHave(text("Home"));
 
         // Find the Pages list
         // Find SoftAssertions
         // Open SoftAssertions
-        $(("div.wiki-rightbar")).$("ul").$$("li").filterBy(text("SoftAssertions")).first().click();
+        $(("div.wiki-rightbar")).$(byText("SoftAssertions")).click();
         $("#wiki-wrapper").$("h1").shouldHave(text("SoftAssertions"));
 
         // Find any example of Junit5
